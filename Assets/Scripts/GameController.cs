a5dbbbc4faa85af9c9bf0288c1d0a629b3696392
Timo <timobleeker@gmail.com>
2016-01-30 12:25:06 +1300

    Add player characters

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameController : MonoBehaviour {
+	
+	public List <PlayerController> playerControllers;
+
+	void Awake() {
+	}
+
+	void Update() {
+
+		Vector3 newPosition;
+
+
+		if (Input.GetButtonDown ("Fire2")) {
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit)) {
+				Debug.DrawLine (ray.origin, ray.direction, Color.green);
+				newPosition = hit.point;
+
+				FindSelectedCharacter().SetNewPosition(newPosition);
+			}
+
+
+		}   
+
+	}
+
+	private PlayerController FindSelectedCharacter()
+	{
+		PlayerController selectedCharacter = playerControllers[0];
+
+		for(int i = 0; i < playerControllers.Count; i++) {
+
+			if(playerControllers[i].Selected == true) {
+				selectedCharacter = playerControllers[i];
+			}
+		}
+
+		return selectedCharacter;
+	}
+
+}
