16713bbf37fbec50b2ad48974ae5d2151167b7cc
Timo <timobleeker@gmail.com>
2016-01-30 17:07:36 +1300

    Adds seed germination and destruction

---
+++ Assets/Scripts/GerminateScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GerminateScript : MonoBehaviour {
+
+	public GameObject[] trees;
+	public Material treeMaterial;
+	
+
+	void OnTriggerEnter(Collider other) {
+
+		Quaternion rotation = Quaternion.AngleAxis(270, Vector3.right);
+
+		int randomIndex = Random.Range(0, trees.Length);
+		GameObject tree = Instantiate(trees[randomIndex], transform.position, rotation) as GameObject;
+		tree.GetComponent<Renderer>().material = treeMaterial;
+
+		Destroy(gameObject);
+
+	}
+}
+
+
