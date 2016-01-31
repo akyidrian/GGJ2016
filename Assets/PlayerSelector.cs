83c75af58bececbfefe316122f696b4299b42a33
Timo <timobleeker@gmail.com>
2016-01-31 14:56:39 +1300

    Add UI

---
+++ Assets/PlayerSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSelector : MonoBehaviour {
+
+	public ControllableObject[] players;
+
+	public void SelectOnClick(int index) 
+	{
+		GameController.instance.SetAllPlayersFalse();
+		players[index].Selected = true;
+	}
+}
